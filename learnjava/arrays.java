52fe5f48415c2fd19cd109323e4be904a2b06181
Kiruthik-Raj <gmkiruthick02@gmail.com>
2024-09-20 16:42:57 +0530

    restart

---
+++ learnjava/arrays.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class arrays {
+    public void pritnArray(int[] nums)
+    {
+        int i = 0;
+        int n = nums.length;
+        for(i = 0;i<n;i++)
+        {
+
+        }
+    }
+    
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        
+    }
+}
